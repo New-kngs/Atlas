b4c728356e1995f4ed3b34bd3d0a6d7e697c824d
byunghoon min <ptsdh123@gmail.com>
2022-07-19 20:08:04 +0900

    사원 수정

--- AltasMES/frmEmployee/frmEmployee.Designer.cs
+++ AltasMES/frmEmployee/frmEmployee.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace AltasMES.frmEmployee
+namespace AltasMES
 {
     partial class frmEmployee
     {
@@ -29,12 +29,77 @@ namespace AltasMES.frmEmployee
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEmployee));
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dgvEmp = new System.Windows.Forms.DataGridView();
+            this.BasePanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.Basepanel4.SuspendLayout();
+            this.Basepanel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.groupBox3);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.Images.SetKeyName(0, "Create.png");
+            this.imageList1.Images.SetKeyName(1, "Modify.png");
+            this.imageList1.Images.SetKeyName(2, "Delete.png");
+            this.imageList1.Images.SetKeyName(3, "Serach.png");
+            this.imageList1.Images.SetKeyName(4, "Execl.png");
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dgvEmp);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox3.Location = new System.Drawing.Point(0, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1034, 491);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // dgvEmp
+            // 
+            this.dgvEmp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvEmp.Location = new System.Drawing.Point(3, 21);
+            this.dgvEmp.Name = "dgvEmp";
+            this.dgvEmp.RowTemplate.Height = 23;
+            this.dgvEmp.Size = new System.Drawing.Size(1028, 467);
+            this.dgvEmp.TabIndex = 0;
+            // 
+            // frmEmployee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1034, 682);
+            this.Name = "frmEmployee";
             this.Text = "frmEmployee";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmEmployee_FormClosing);
+            this.Load += new System.EventHandler(this.frmEmployee_Load);
+            this.BasePanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.Basepanel4.ResumeLayout(false);
+            this.Basepanel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmp)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dgvEmp;
     }
 }
