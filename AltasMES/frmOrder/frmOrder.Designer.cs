4cbff2cc09a1744b4324c87a67ad9c2ff653ccbf
이진형 <jehl0227@naver.com>
2022-07-27 01:05:03 +0900

    ㅇ

---
+++ AltasMES/frmOrder/frmOrder.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace AltasMES
+{
+    partial class frmOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmOrder));
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dgvWH = new System.Windows.Forms.DataGridView();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.dgvPDT = new System.Windows.Forms.DataGridView();
+            this.BasePanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.Basepanel4.SuspendLayout();
+            this.Basepanel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvWH)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPDT)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.groupBox4);
+            this.panel2.Location = new System.Drawing.Point(0, 436);
+            this.panel2.Size = new System.Drawing.Size(1034, 246);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Size = new System.Drawing.Size(1034, 256);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox2.Text = "검색조건";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.Images.SetKeyName(0, "Create.png");
+            this.imageList1.Images.SetKeyName(1, "Modify.png");
+            this.imageList1.Images.SetKeyName(2, "Delete.png");
+            this.imageList1.Images.SetKeyName(3, "Serach.png");
+            this.imageList1.Images.SetKeyName(4, "Execl.png");
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Size = new System.Drawing.Size(98, 51);
+            this.lblTitle.Text = "주문";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dgvWH);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox3.Location = new System.Drawing.Point(0, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1034, 256);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "주문 목록";
+            // 
+            // dgvWH
+            // 
+            this.dgvWH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvWH.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvWH.Location = new System.Drawing.Point(3, 25);
+            this.dgvWH.Name = "dgvWH";
+            this.dgvWH.RowTemplate.Height = 23;
+            this.dgvWH.Size = new System.Drawing.Size(1028, 228);
+            this.dgvWH.TabIndex = 1;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.dgvPDT);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox4.Location = new System.Drawing.Point(0, 0);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(1034, 246);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "주문 처리 현황";
+            // 
+            // dgvPDT
+            // 
+            this.dgvPDT.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPDT.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvPDT.Location = new System.Drawing.Point(3, 25);
+            this.dgvPDT.Name = "dgvPDT";
+            this.dgvPDT.RowTemplate.Height = 23;
+            this.dgvPDT.Size = new System.Drawing.Size(1028, 218);
+            this.dgvPDT.TabIndex = 0;
+            // 
+            // frmOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 682);
+            this.Name = "frmOrder";
+            this.Text = "주문";
+            this.BasePanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.Basepanel4.ResumeLayout(false);
+            this.Basepanel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvWH)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPDT)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dgvWH;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.DataGridView dgvPDT;
+    }
+}
