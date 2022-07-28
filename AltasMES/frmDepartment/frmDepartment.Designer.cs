41064a0d16b48225dbc42ffeed8133809346045b
min <min@DESKTOP-JJMJOMO>
2022-07-28 12:52:47 +0900

    Revert "Revert "부서 등록""
    This reverts commit 73fdcf578a4fde5ee3d1f49ff6d50d2047aab034.

---
+++ AltasMES/frmDepartment/frmDepartment.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace AltasMES
+{
+    partial class frmDepartment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDepartment));
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dgvdept = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSerach = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.BasePanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.Basepanel4.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.Basepanel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvdept)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.groupBox3);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblSearch);
+            this.groupBox2.Controls.Add(this.btnSearch);
+            this.groupBox2.Controls.Add(this.txtSerach);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.Images.SetKeyName(0, "Create.png");
+            this.imageList1.Images.SetKeyName(1, "Modify.png");
+            this.imageList1.Images.SetKeyName(2, "Delete.png");
+            this.imageList1.Images.SetKeyName(3, "Serach.png");
+            this.imageList1.Images.SetKeyName(4, "Execl.png");
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dgvdept);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1034, 491);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // dgvdept
+            // 
+            this.dgvdept.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvdept.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvdept.Location = new System.Drawing.Point(3, 17);
+            this.dgvdept.Name = "dgvdept";
+            this.dgvdept.RowTemplate.Height = 23;
+            this.dgvdept.Size = new System.Drawing.Size(1028, 471);
+            this.dgvdept.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSearch.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSearch.ImageIndex = 3;
+            this.btnSearch.ImageList = this.imageList1;
+            this.btnSearch.Location = new System.Drawing.Point(299, 40);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.btnSearch.Size = new System.Drawing.Size(78, 29);
+            this.btnSearch.TabIndex = 14;
+            this.btnSearch.Text = "조회";
+            this.btnSearch.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // txtSerach
+            // 
+            this.txtSerach.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSerach.Location = new System.Drawing.Point(76, 40);
+            this.txtSerach.Name = "txtSerach";
+            this.txtSerach.Size = new System.Drawing.Size(215, 29);
+            this.txtSerach.TabIndex = 13;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblSearch.Location = new System.Drawing.Point(12, 43);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(58, 21);
+            this.lblSearch.TabIndex = 15;
+            this.lblSearch.Text = "부서명";
+            // 
+            // frmDepartment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 682);
+            this.Name = "frmDepartment";
+            this.Text = "부서";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmDepartment_FormClosing);
+            this.Load += new System.EventHandler(this.frmDepartment_Load);
+            this.BasePanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.Basepanel4.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.Basepanel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvdept)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dgvdept;
+        protected System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSerach;
+        private System.Windows.Forms.Label lblSearch;
+    }
+}
---
+++ AtlasMVCAPI/Controllers/ApiControllers/DepartmentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AtlasDTO;
+using AtlasMVCAPI.Models;
+
+namespace AtlasMVCAPI.Controllers
+{
+
+    [RoutePrefix("api/Department")]
+    public class DepartmentController : ApiController
+    {
+        //https://localhost:44391/api/Department/all
+        [Route("all")]
+        public IHttpActionResult GetDepartmentAll()
+        {
+            try
+            {
+                DepartmentDAC db = new DepartmentDAC();
+                List<DepartmentVO> list = db.GetDepartmentAll();
+
+                ResMessage<List<DepartmentVO>> result = new ResMessage<List<DepartmentVO>>()
+                {
+                    ErrCode = (list == null) ? -9 : 0,
+                    ErrMsg = (list == null) ? "조회중 오류발생" : "S",
+                    Data = list
+                };
+
+                return Ok(result);
+            }
+            catch (Exception err)
+            {
+                System.Diagnostics.Debug.WriteLine(err.Message);
+
+                return Ok(new ResMessage()
+                {
+                    ErrCode = -9,
+                    ErrMsg = "서비스 관리자에게 문의하시기 바랍니다."
+                });
+            }
+        }
+
+
+    }
+}
