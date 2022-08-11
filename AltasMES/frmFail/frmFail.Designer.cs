8e7bea815d9797ffa567e1e909b6072f77332a80
gs2566 <gs2566@office.uc.ac.kr>
2022-08-11 19:10:30 +0900

    불량수정중

--- AltasMES/frmFail/frmFail.Designer.cs
+++ AltasMES/frmFail/frmFail.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace AltasMES
+namespace AltasMES.frmFail
 {
     partial class frmFail
     {
@@ -29,62 +29,7 @@ namespace AltasMES
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFail));
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.dgvList = new System.Windows.Forms.DataGridView();
-            this.BasePanel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.Basepanel4.SuspendLayout();
-            this.Basepanel2.SuspendLayout();
-            this.groupBox1.SuspendLayout();
-            this.groupBox3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.groupBox3);
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.groupBox2.Text = "검색조건";
-            // 
-            // imageList1
-            // 
-            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
-            this.imageList1.Images.SetKeyName(0, "Create.png");
-            this.imageList1.Images.SetKeyName(1, "Modify.png");
-            this.imageList1.Images.SetKeyName(2, "Delete.png");
-            this.imageList1.Images.SetKeyName(3, "Serach.png");
-            this.imageList1.Images.SetKeyName(4, "Execl.png");
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.Size = new System.Drawing.Size(98, 51);
-            this.lblTitle.Text = "불량";
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.dgvList);
-            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold);
-            this.groupBox3.Location = new System.Drawing.Point(0, 0);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(1034, 491);
-            this.groupBox3.TabIndex = 0;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "groupBox3";
-            // 
-            // dgvList
-            // 
-            this.dgvList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvList.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvList.Location = new System.Drawing.Point(3, 25);
-            this.dgvList.Name = "dgvList";
-            this.dgvList.RowTemplate.Height = 23;
-            this.dgvList.Size = new System.Drawing.Size(1028, 463);
-            this.dgvList.TabIndex = 0;
             // 
             // frmFail
             // 
@@ -92,24 +37,11 @@ namespace AltasMES
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1034, 682);
             this.Name = "frmFail";
-            this.Tag = "";
-            this.Text = "불량현황";
-            this.Load += new System.EventHandler(this.frmFail_Load);
-            this.BasePanel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.Basepanel4.ResumeLayout(false);
-            this.Basepanel2.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvList)).EndInit();
+            this.Text = "frmFail";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.DataGridView dgvList;
     }
 }
